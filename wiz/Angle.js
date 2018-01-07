0d4312a28b4eed905d45171538a34d09d4273759
songgz <sgzhe@163.com>
2018-01-07 10:02:44 +0800

    added thl314.html

--- wiz/Angle.js
+++ wiz/Angle.js
@@ -3,9 +3,9 @@ var Lunisolar = (function(global){
     var angle = global.Angle = global.Angle || function (rad) {
         //this.rad = Angle.inPI2(rad);
     };
-    angle.D2R = Math.PI / 180.0;
-    angle.R2D = 180.0 / Math.PI;
     angle.PI2 = 2 * Math.PI;
+    angle.D2R = Math.PI / 180.0;
+    angle.R2D = 180.0 / Math.PI;    
     angle.R2A = 180 * 3600 / Math.PI;  //3600.0 * (180 / Math.PI); //每弧度的角秒数
 
     angle.inPI2 = function (rad) {
@@ -15,6 +15,12 @@ var Lunisolar = (function(global){
         }
         return rad;
     };
+
+    var Angle = function(rad){
+        this.rad = rad;
+    };
+
+    
 
     return global;
 })(Lunisolar || {});
