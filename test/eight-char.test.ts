2d13bcb78d360878d3276a29509150733ee5f61a
songgz <sgzhe@163.com>
2021-12-22 15:42:36 +0800

    add eight-char

--- test/eight-char.test.ts
+++ test/eight-char.test.ts
@@ -1,10 +1,12 @@
 import {EightChar} from "../src/eight-char";
+import {JDate} from "../src/j-date";
 
 
 describe('EightChar Class', () => {
     test('new method', () => {
         //2014-4-23 10:37:40   //甲午年 戊辰月 甲子日 己巳时 真太阳 10:24:48
-        let ec = new EightChar(2456770.942824074);
+        let jd = JDate.gd2jd(2014, 4, 23, 10,37,40); //2456770.942824074
+        let ec = new EightChar(jd);
         expect(ec.getYear()).toBe('甲午');
         expect(ec.getMonth()).toBe('戊辰');
         expect(ec.getDay()).toBe('甲子');
