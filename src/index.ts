a95fe5329c8c3dfc6796a94fa360650f4692115a
songgz <sgzhe@163.com>
2021-12-29 08:06:17 +0800

    move jasmine

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-
+import {SolarDate} from "./solar-date";
+import {Angle} from "./angle";
+import {Earth, Moon, MoonPhase, Nutation, Sun} from "./ephem";
+import {SolarTerm} from "./solar-term";
+import {EightChar} from "./eight-char";
 import {LunarDate} from "./lunar-date";
+import {JulianDate} from "./julian-date";
+
+module.exports = {
+    Angle,
+    Nutation,
+    Earth,
+    Sun,
+    Moon,
+    MoonPhase,
+    SolarTerm,
+    EightChar,
+    SolarDate,
+    LunarDate,
+    JulianDate
+};
 
 
